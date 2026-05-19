be69a0fbc6ee905da32a79d521026b017e5ad223
andrey baidarka <andrejmedvedev2713@gmail.com>
2026-05-19 10:42:30 +0500

    Расширил инит

--- backend/src/iam/routers/__init__.py
+++ backend/src/iam/routers/__init__.py
@@ -3,7 +3,9 @@ __all__ = ["router"]
 from fastapi import APIRouter
 
 from .auth import auth_router
+from .invitation import invitation_router
 
 router = APIRouter()  # noqa: RUF067
 
 router.include_router(auth_router)  # noqa: RUF067
+router.include_router(invitation_router)  # noqa: RUF067
