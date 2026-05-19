031d240ca16d0410994b6690cc1fa36d543b76c0
andrey baidarka <andrejmedvedev2713@gmail.com>
2026-05-19 10:41:53 +0500

    Создал сервисы для создания класов

---
+++ backend/src/iam/domain/services.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+from uuid import UUID
+
+from pydantic import EmailStr, SecretStr
+
+from ...shared.utils.time import get_expiration_time
+from ..security import hash_password
+from .dataclasses import Invitation, User
+from .vo import Username, UserRole
+
+INVITATION_EXPIRES_IN_DAYS = 7
+
+
+def create_user(
+    email: EmailStr, username: str, password: str, role: UserRole = UserRole.USER
+) -> User:
+    return User(
+        username=Username(username),
+        email=email,
+        password_hash=SecretStr(hash_password(password)),
+        role=role,
+    )
+
+
+def create_invitation(
+    email: EmailStr, role: UserRole = UserRole.USER, invited_by: UUID | None = None
+) -> Invitation:
+    return Invitation(
+        email=email,
+        expires_at=get_expiration_time(timedelta(days=7)),
+        assigned_role=role,
+        invited_by=invited_by,
+    )
