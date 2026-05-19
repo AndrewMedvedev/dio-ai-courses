9aa25b67226e40e340c262bc1635293903a28f4e
andrey baidarka <andrejmedvedev2713@gmail.com>
2026-05-19 10:39:22 +0500

    Добавил два поля в приглашение, отправленно и роль, так же в модель пользователя добавил роль

--- backend/src/iam/database/models.py
+++ backend/src/iam/database/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from uuid import UUID
 
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ...core.database import Base
+from ..domain.vo import UserRole
 
 
 class UserOrm(Base):
@@ -12,6 +13,7 @@ class UserOrm(Base):
     username: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True)
     password_hash: Mapped[str] = mapped_column(unique=True)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole))
     is_verify: Mapped[bool] = mapped_column(default=False)
 
 
@@ -21,6 +23,8 @@ class InvitationOrm(Base):
     email: Mapped[str]
     token: Mapped[str] = mapped_column(unique=True)
     invited_by: Mapped[UUID | None] = mapped_column(nullable=True)
+    assigned_role: Mapped[UserRole] = mapped_column(Enum(UserRole))
     expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     used_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
+    is_delivered: Mapped[bool] = mapped_column(default=True)
     is_used: Mapped[bool]
